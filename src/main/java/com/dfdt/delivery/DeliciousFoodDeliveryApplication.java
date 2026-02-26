5c09ec0477233d99124e591038cd705012dc63dc
unknown <hanleejss012@naver.com>
2026-02-26 14:34:14 +0900

    Project generate

---
+++ src/main/java/com/dfdt/delivery/DeliciousFoodDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.dfdt.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliciousFoodDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliciousFoodDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dfdt/delivery/DeliciousFoodDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dfdt.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliciousFoodDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
