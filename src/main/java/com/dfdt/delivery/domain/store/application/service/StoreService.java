1a4c567aa5bfb0c317f469f339ad6e1a105985ee
Lim JinKeon <0907john24@gmail.com>
2026-03-04 14:42:12 +0900

    feat: 비즈니스 로직 및 REPO 작성

--- src/main/java/com/dfdt/delivery/domain/store/application/service/StoreService.java
+++ src/main/java/com/dfdt/delivery/domain/store/application/service/StoreService.java
@@ -1,6 +1,17 @@
 package com.dfdt.delivery.domain.store.application.service;
 
+import com.dfdt.delivery.common.exception.BusinessException;
+import com.dfdt.delivery.domain.category.domain.entity.Category;
+import com.dfdt.delivery.domain.category.domain.enums.CategoryErrorCode;
 import com.dfdt.delivery.domain.category.domain.repository.CategoryRepository;
+import com.dfdt.delivery.domain.region.domain.entity.Region;
+import com.dfdt.delivery.domain.region.domain.enums.RegionErrorCode;
+import com.dfdt.delivery.domain.region.domain.repository.RegionRepository;
+import com.dfdt.delivery.domain.store.domain.entity.Store;
+import com.dfdt.delivery.domain.store.domain.entity.StoreCategory;
+import com.dfdt.delivery.domain.store.domain.entity.StoreRating;
+import com.dfdt.delivery.domain.store.domain.enums.StoreErrorCode;
+import com.dfdt.delivery.domain.store.domain.enums.StoreStatus;
 import com.dfdt.delivery.domain.store.domain.repository.StoreCategoryRepository;
 import com.dfdt.delivery.domain.store.domain.repository.StoreCustomRepository;
 import com.dfdt.delivery.domain.store.domain.repository.StoreRatingRepository;
@@ -8,11 +19,6 @@ import com.dfdt.delivery.domain.store.domain.repository.StoreRepository;
 import com.dfdt.delivery.domain.store.presentation.dto.request.StoreCreateReqDto;
 import com.dfdt.delivery.domain.store.presentation.dto.request.StoreStatusReqDto;
 import com.dfdt.delivery.domain.store.presentation.dto.request.StoreUpdateReqDto;
-import com.dfdt.delivery.domain.category.domain.entity.Category;
-import com.dfdt.delivery.domain.store.domain.entity.Store;
-import com.dfdt.delivery.domain.store.domain.entity.StoreCategory;
-import com.dfdt.delivery.domain.store.domain.entity.StoreRating;
-import com.dfdt.delivery.domain.store.domain.enums.StoreStatus;
 import com.dfdt.delivery.domain.store.presentation.dto.response.*;
 import com.dfdt.delivery.domain.user.entity.User;
 import com.dfdt.delivery.domain.user.enums.UserRole;
@@ -37,13 +43,11 @@ public class StoreService {
     private final CategoryRepository categoryRepository;
     private final StoreCategoryRepository storeCategoryRepository;
     private final StoreRatingRepository storeRatingRepository;
+    private final RegionRepository regionRepository;
 
     @Transactional(readOnly = true)
     public StoreResDto getStore(UUID storeId) {
-        Store store = storeRepository.findByStoreIdAndDeletedAtIsNull(storeId)
-                .filter(s -> s.getRegion().getIsOrderEnabled())
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않거나 삭제된 가게입니다."));
-
+        Store store = findStoreById(storeId);
         StoreRating rating = storeRatingRepository.findById(storeId).orElse(null);
 
         return StoreResDto.from(store, rating);
@@ -51,21 +55,32 @@ public class StoreService {
 
     @Transactional(readOnly = true)
     public Page<StoreResDto> getStores(int page, int size, String sortBy, boolean isAsc, UUID category, String name) {
-        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
-        Sort sort = Sort.by(direction, sortBy);
-        Pageable pageable = PageRequest.of(page, size, sort);
+        Pageable pageable = createPageable(page, size, sortBy, isAsc);
+        Page<StoreResDto> storeResDto = storeCustomRepository.searchStores(pageable, category, name);
 
-        return storeCustomRepository.searchStores(pageable, category, name);
+        checkStores(storeResDto.getTotalElements());
+
+        return storeResDto;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<StoreAdminResDto> getStoresAdmin(int page, int size, String sortBy, boolean isAsc, UUID category, String name, Boolean isDeleted) {
+        Pageable pageable = createPageable(page, size, sortBy, isAsc);
+        Page<StoreAdminResDto> storeAdminResDto = storeCustomRepository.searchStoresAdmin(pageable, category, name, isDeleted);
+
+        checkStores(storeAdminResDto.getTotalElements());
+
+        return storeAdminResDto;
     }
 
     public StoreCreateResDto createStore(StoreCreateReqDto request, User user) {
         List<Category> categories = categoryRepository.findAllById(request.getCategoryIds());
-        if (categories.isEmpty() || categories.size() != request.getCategoryIds().size()) {
-            throw new IllegalArgumentException("해당 카테고리가 존재하지 않습니다.");
-        }
 
-        Store store = Store.create(request, user);
-        store.addCategories(categories);
+        checkExistCategory(request.getCategoryIds().size(), categories);
+        Region region = regionRepository.findById(request.getRegionId()).orElseThrow(() -> new BusinessException(RegionErrorCode.NOT_FOUND_REGION));
+
+        Store store = Store.create(request, user, region);
+        store.addCategories(categories, user.getUsername());
         storeRepository.save(store);
 
         return StoreCreateResDto.from(store);
@@ -73,76 +88,126 @@ public class StoreService {
 
     public StoreUpdateResDto updateStore(UUID storeId, StoreUpdateReqDto request, User user) {
         List<Category> newCategories = categoryRepository.findAllById(request.getCategoryIds());
-        if (newCategories.isEmpty() || newCategories.size() != request.getCategoryIds().size()) {
-            throw new IllegalArgumentException("해당 카테고리가 존재하지 않습니다.");
-        }
-        Store store = storeRepository.findById(storeId).orElseThrow(() -> new IllegalArgumentException("해당 가게를 찾을 수 없습니다."));
-        if (!store.getUser().getUsername().equals(user.getUsername()) || user.getRole().equals(UserRole.OWNER)) {
-            throw new SecurityException("본인 소유 가게만 수정할 수 있습니다.");
-        }
-
+        Store store = findStoreById(storeId);
         List<StoreCategory> before = storeCategoryRepository.findByStore(store);
 
-        // 1. 기존 active 카테고리
-//        List<StoreCategory> existing = before.stream()
-//                .filter(sc -> sc.getDeletedAt() == null)
-//                .toList();
-//
-//        // 2. 삭제 처리 (기존에는 있는데 새 리스트에는 없는 경우)
-//        existing.stream()
-//                .filter(sc -> !newCategories.contains(sc.getCategory()))
-//                .forEach(sc -> sc.deleteStoreCategory(user)
-//                );
-//
-//        // 3. 새로 추가 (새 리스트에는 있는데 기존에는 없는 경우)
-//        newCategories.stream()
-//                .filter(c -> existing.stream().noneMatch(sc -> sc.getCategory().equals(c)))
-//                .forEach(store::addCategory);
+        checkExistCategory(request.getCategoryIds().size(), newCategories);
+        checkMyStore(user, store);
+        checkDeletedStore(store);
 
-        store.update(request);
+        // 1. 기존 active 카테고리
+        List<StoreCategory> existing = before.stream()
+                .filter(sc -> sc.getSoftDeleteAudit() == null)
+                .toList();
+
+        // 2. 삭제 처리 (기존에는 있는데 새 리스트에는 없는 경우)
+        existing.stream()
+                .filter(sc -> !newCategories.contains(sc.getCategory()))
+                .forEach(sc -> sc.delete(user.getUsername())
+                );
+
+        // 3. 새로 추가 (새 리스트에는 있는데 기존에는 없는 경우)
+        newCategories.stream()
+                .filter(c -> existing.stream().noneMatch(sc -> sc.getCategory().equals(c)))
+                .forEach(c -> store.addCategory(c, user.getUsername()));
+
+        store.update(request, user.getUsername());
 
         return StoreUpdateResDto.from(store);
     }
 
     public void deleteStore(UUID storeId, User user) {
-        Store store = storeRepository.findById(storeId).orElseThrow(() -> new IllegalArgumentException("해당 가게를 찾을 수 없습니다."));
-        if (!store.getUser().getUsername().equals(user.getUsername()) || user.getRole().equals(UserRole.OWNER)) {
-            throw new SecurityException("본인 소유 가게만 삭제할 수 있습니다.");
+        Store store = findStoreById(storeId);
+        checkMyStore(user, store);
+
+        if (store.getSoftDeleteAudit() == null) {
+            throw new BusinessException(StoreErrorCode.ALREADY_DELETED);    // 삭제된 가게인지 확인
         }
 
-//        store.delete(user.getUsername());
+        store.delete(user.getUsername());
     }
 
     public void changeIsOpen(UUID storeId, User user) {
-        Store store = storeRepository.findById(storeId).orElseThrow(() -> new IllegalArgumentException("해당 가게를 찾을 수 없습니다."));
-        if (!store.getUser().getUsername().equals(user.getUsername()) || user.getRole().equals(UserRole.OWNER)) {
-            throw new SecurityException("본인 소유 가게만 영업 상태를 변경할 수 있습니다.");
-        }
+        Store store = findStoreById(storeId);
+        checkMyStore(user, store);
+        checkDeletedStore(store);
 
-        store.changeIsOpen();
+        store.changeIsOpen(user.getUsername());
     }
 
     public List<MyStoreResDto> getMyStores(String username) {
         return storeRepository
-                .findByUsername(username)
+                .findByUser_UsernameOrderByCreateAuditAsc(username)
                 .stream()
                 .map(MyStoreResDto::from)
                 .toList();
     }
 
-    public void restoreStore(UUID storeId) {
-        Store store = storeRepository.findById(storeId).orElseThrow(() -> new IllegalArgumentException("해당 가게를 찾을 수 없습니다."));
+    public void restoreStore(UUID storeId, User user) {
+        // 영업 중지된 가게인지 확인
+        Store store = findStoreById(storeId);
         if (!store.getStatus().equals(StoreStatus.SUSPENDED)) {
-            throw new IllegalArgumentException("영업 중지된 가게가 아닙니다.");
+            throw new BusinessException(StoreErrorCode.NOT_SUSPENDED);
         }
 
-        store.restore();
+        store.restore(user.getUsername());
     }
 
-    public StoreStatusResDto changeStatus(UUID storeId, StoreStatusReqDto request) {
-        Store store = storeRepository.findById(storeId).orElseThrow(() -> new IllegalArgumentException("해당 가게를 찾을 수 없습니다."));
-        store.changeStatus(request.getStatus());
+    public StoreStatusResDto changeStatus(UUID storeId, StoreStatusReqDto request, User user) {
+        Store store = findStoreById(storeId);
+        store.changeStatus(request.getStatus(), user.getUsername());
 
         return StoreStatusResDto.from(store);
     }
+
+    public List<StoreStatusRequestResDto> getRequestedStores() {
+        List<Store> stores = storeRepository.findStoresByStatusNotDeleted(StoreStatus.REQUESTED);
+        if (stores.isEmpty()) {
+            return null;
+        }
+
+        return stores.stream()
+                .map(StoreStatusRequestResDto::from)
+                .toList();
+    }
+
+    private Pageable createPageable(int page, int size, String sortBy, boolean isAsc) {
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
+        return PageRequest.of(page, size, sort);
+    }
+
+    // 해당 가게가 존재하는지 확인
+    private Store findStoreById(UUID storeId) {
+        return storeRepository.findById(storeId).orElseThrow(() -> new BusinessException(StoreErrorCode.NOT_FOUND_STORE));
+    }
+
+    // 해당 카테고리가 존재하는지 확인
+    private void checkExistCategory(Integer size, List<Category> categories) {
+        if (categories.isEmpty() || categories.size() != size) {
+            throw new BusinessException(CategoryErrorCode.NOT_FOUND_CATEGORY);
+        }
+    }
+
+    // 등록된 가게가 있는지 확인
+    private static void checkStores(long totalElements) {
+        if (totalElements == 0) {
+            throw new BusinessException(StoreErrorCode.NOT_FOUND_STORES);
+        }
+    }
+
+    // 본인 소유의 가게만 정보 변경 가능
+    private static void checkMyStore(User user, Store store) {
+        if (!store.getUser().getUsername().equals(user.getUsername()) && !user.getRole().equals(UserRole.MASTER)) {
+            throw new BusinessException(StoreErrorCode.NOT_MY_STORE);
+        }
+    }
+
+    // 삭제된 가게는 정보 변경 X
+    private static void checkDeletedStore(Store store) {
+        if (store.getSoftDeleteAudit() != null) {
+            throw new BusinessException(StoreErrorCode.NOT_MODIFIED);
+        }
+    }
+
 }
--- src/main/java/com/dfdt/delivery/domain/store/domain/repository/StoreCustomRepository.java
+++ src/main/java/com/dfdt/delivery/domain/store/domain/repository/StoreCustomRepository.java
@@ -1,5 +1,6 @@
 package com.dfdt.delivery.domain.store.domain.repository;
 
+import com.dfdt.delivery.domain.store.presentation.dto.response.StoreAdminResDto;
 import com.dfdt.delivery.domain.store.presentation.dto.response.StoreResDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,4 +9,6 @@ import java.util.UUID;
 
 public interface StoreCustomRepository {
     Page<StoreResDto> searchStores(Pageable pageable, UUID category, String name);
+
+    Page<StoreAdminResDto> searchStoresAdmin(Pageable pageable, UUID category, String name, Boolean isDeleted);
 }
