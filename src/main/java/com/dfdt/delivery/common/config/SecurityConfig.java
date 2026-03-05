26485cf89ce3dc0a2f44007ae3815711194c9b36
MKJ-One <hanleejss012@naver.com>
2026-03-05 16:20:46 +0900

    refactor : 필터-중복 로그인 확인

--- src/main/java/com/dfdt/delivery/common/config/SecurityConfig.java
+++ src/main/java/com/dfdt/delivery/common/config/SecurityConfig.java
@@ -53,6 +53,10 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/api/v1/auth/**",
+                                "/api/v1/swagger",
+                                "/api/v1/swagger-ui/**",
+                                "/api/v1/v3/api-docs/**",
+                                "/api/v1/api-docs/**",
                                 "/swagger",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
