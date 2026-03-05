6e1302c8d6036194cf61f89b87b261d20e780bc5
Lim JinKeon <0907john24@gmail.com>
2026-03-05 14:15:34 +0900

    feat: Store Repo 확장성 고려한 설계 변경

---
+++ src/main/java/com/dfdt/delivery/domain/store/application/service/command/StoreCommandService.java
@@ -0,0 +1,29 @@
+package com.dfdt.delivery.domain.store.application.service.command;
+
+import com.dfdt.delivery.domain.auth.infrastructure.security.CustomUserDetails;
+import com.dfdt.delivery.domain.store.presentation.dto.request.StoreCreateReqDto;
+import com.dfdt.delivery.domain.store.presentation.dto.request.StoreStatusReqDto;
+import com.dfdt.delivery.domain.store.presentation.dto.request.StoreUpdateReqDto;
+import com.dfdt.delivery.domain.store.presentation.dto.response.MyStoreResDto;
+import com.dfdt.delivery.domain.store.presentation.dto.response.StoreCreateResDto;
+import com.dfdt.delivery.domain.store.presentation.dto.response.StoreStatusResDto;
+import com.dfdt.delivery.domain.store.presentation.dto.response.StoreUpdateResDto;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface StoreCommandService {
+    StoreCreateResDto createStore(StoreCreateReqDto request, CustomUserDetails userDetails);
+
+    StoreUpdateResDto updateStore(UUID storeId, StoreUpdateReqDto request, CustomUserDetails userDetails);
+
+    void deleteStore(UUID storeId, CustomUserDetails user);
+
+    void changeIsOpen(UUID storeId, CustomUserDetails userDetails);
+
+    List<MyStoreResDto> getMyStores(String username);
+
+    void restoreStore(UUID storeId, CustomUserDetails user);
+
+    StoreStatusResDto changeStatus(UUID storeId, StoreStatusReqDto request, CustomUserDetails userDetails);
+}
--- src/main/java/com/dfdt/delivery/domain/store/domain/repository/StoreRepository.java
+++ src/main/java/com/dfdt/delivery/domain/store/domain/repository/StoreRepository.java
@@ -2,32 +2,17 @@ package com.dfdt.delivery.domain.store.domain.repository;
 
 import com.dfdt.delivery.domain.store.domain.entity.Store;
 import com.dfdt.delivery.domain.store.domain.enums.StoreStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface StoreRepository extends JpaRepository<Store, UUID> {
+public interface StoreRepository {
     List<Store> findByUser_UsernameOrderByCreateAuditAsc(String username);
 
-    @Query("SELECT s FROM Store s WHERE s.storeId = :storeId AND s.softDeleteAudit.deletedAt IS NULL")
     Optional<Store> findByStoreIdAndNotDeleted(UUID storeId);
 
-    @Query("""
-        SELECT CASE WHEN COUNT(s) > 0 THEN true ELSE false END
-        FROM Store s JOIN s.categories sc
-        WHERE sc.category.categoryId = :categoryId
-          AND s.softDeleteAudit.deletedAt IS NULL
-    """)
     boolean existsByCategoryIdAndNotDeleted(UUID categoryId);
 
-    @Query("""
-        SELECT s
-        FROM Store s
-        WHERE s.status = :status
-          AND s.softDeleteAudit.deletedAt IS NULL
-    """)
     List<Store> findStoresByStatusNotDeleted(StoreStatus status);
 }
