6ed4ed45c4d67b695d06650e3915e1b52a11db12
gh9727 <gj9727@gmail.com>
2026-03-06 14:11:09 +0900

    git_action dev 배포 테스트용 탄력적 IP 연결 및 테스트용 Test 디렉 설정

--- src/main/java/com/dfdt/delivery/test/controller/TestController.java
+++ src/main/java/com/dfdt/delivery/test/controller/TestController.java
@@ -1,4 +1,12 @@
 package com.dfdt.delivery.test.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+    @GetMapping("/test_1")
+    public String getTest01() {
+        return "test_01";
+    }
 }
