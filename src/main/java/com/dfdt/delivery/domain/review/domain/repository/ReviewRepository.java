4ba60bdceeecf235303d526721463f48e6428887
dnjswns98 <tngkrt2004@gmail.com>
2026-03-07 23:10:42 +0900

    feat: ReviewRepository 추가

--- src/main/java/com/dfdt/delivery/domain/review/domain/repository/ReviewRepository.java
+++ src/main/java/com/dfdt/delivery/domain/review/domain/repository/ReviewRepository.java
@@ -1,4 +1,14 @@
 package com.dfdt.delivery.domain.review.domain.repository;
 
+import com.dfdt.delivery.domain.review.domain.entity.Review;
+
+import java.util.Optional;
+import java.util.UUID;
+
 public interface ReviewRepository {
+    Review save(Review review);
+
+    Optional<Review> findById(UUID reviewId);
+
+    boolean existsByOrderIdAndSoftDeleteAuditDeletedAtIsNull(UUID orderId);
 }
