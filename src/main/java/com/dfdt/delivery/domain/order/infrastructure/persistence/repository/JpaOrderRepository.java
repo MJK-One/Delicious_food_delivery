41f566e8eccb926b7d86527877bed7199c1af9dd
yujin <yujin09219@naver.com>
2026-03-07 11:16:05 +0900

    refactor : 레포 상속 구조 변경

--- src/main/java/com/dfdt/delivery/domain/order/infrastructure/persistence/repository/JpaOrderRepository.java
+++ src/main/java/com/dfdt/delivery/domain/order/infrastructure/persistence/repository/JpaOrderRepository.java
@@ -1,7 +1,6 @@
 package com.dfdt.delivery.domain.order.infrastructure.persistence.repository;
 
 import com.dfdt.delivery.domain.order.domain.entity.Order;
-import com.dfdt.delivery.domain.order.domain.repository.OrderRepository;
 import com.dfdt.delivery.domain.payment.domain.entity.Payment;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,14 +10,12 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface JpaOrderRepository extends JpaRepository<Order, UUID> , OrderRepository {
+public interface JpaOrderRepository extends JpaRepository<Order, UUID> {
 
-    @Override
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select o from Order o where o.orderId = :orderId")
     Optional<Order> findByIdWithLock(UUID orderId);
 
-    @Override
     @Query("select p from Payment  p where  p.orderId = :orderId")
     Optional<Payment> findPaymentOrderId(UUID orderId);
 }
